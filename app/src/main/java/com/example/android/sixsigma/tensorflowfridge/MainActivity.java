08aff98326168d5456bed0f79723a710ea3678a2
FSA\sd.nkansah <Jesse.Offei-NkansahSD@mtsu.edu>
2016-11-24 17:21:13 -0600

    minor changes

--- app/src/main/java/com/example/android/sixsigma/tensorflowfridge/MainActivity.java
+++ app/src/main/java/com/example/android/sixsigma/tensorflowfridge/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.android.sixsigma.tensorflowfridge;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static ListView fridgeList;
+    public static Button newItemButton;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fridgeList = (ListView) findViewById(R.id.fridge_list);
+        newItemButton = (Button) findViewById(R.id.new_item_button);
+
+        newItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
+
 }
